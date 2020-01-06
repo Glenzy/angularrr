0a95fc3a3a3c8b0a607e7495c9b57d13721989a2
Glen Coffey <glenrcoffey@gmail.com>
2020-01-07 07:56:48 +1100

    second commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule, MatButtonModule } from '@angular/material';
+
+import { AppComponent } from './app.component';
+import { QuestionFormComponent } from './question-form/question-form.component';
+import { ResultsComponent } from './results/results.component';
+
+import { RouterModule, Routes } from '@angular/router';
+import { QuestionsComponent } from './questions/questions.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const appRoutes: Routes =[
+  { path: 'welcome', component: WelcomeComponent },
+  { path: ':quizId', component: QuestionsComponent },
+  { path: '', redirectTo: "welcome", pathMatch: "prefix"},
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuestionFormComponent,
+    ResultsComponent,
+    QuestionsComponent,
+    WelcomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/quiz.model.ts
@@ -0,0 +1,15 @@
+export class Choice {
+  constructor(public value: string, public correct?: boolean) {}
+}
+
+export class Question {
+  constructor(public label: string, public choices: Choice[]){}
+}
+
+export class Quiz {
+  constructor(public label: string, public name: string, public description: string, public fileName: string){}
+}
+
+export class Answers {
+  constructor(public values: Choice[] = []) {}
+}
